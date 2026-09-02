c8a6b8ee9d36758cff299c8c77380b9c8511cd94
supportcrypgo <support.crypgo@gmail.com>
2026-09-02 23:52:16 +0200

    Fix frontend Render build CSS handling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
 import "./globals.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import ClientLayout from "./ClientLayout";
 
 export const dynamic = 'force-dynamic';
