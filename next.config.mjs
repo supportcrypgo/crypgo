c55c98a426982a58d1d4a29869f9ed7e8e5f11e5
Osagie Great Ayemere <mrosagiegreat@gmail.com>
2026-09-02 22:25:29 +0200

    Configure webpack alias for Render

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+import path from "node:path";
+
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === "production";
 const useGithubPagesPath = process.env.NEXT_PUBLIC_BASE_PATH === "/Crypgo";
@@ -6,6 +8,13 @@ const nextConfig = {
   skipTrailingSlashRedirect: true,
   basePath: isProd && useGithubPagesPath ? "/Crypgo" : "",
   assetPrefix: isProd && useGithubPagesPath ? "/Crypgo/" : "",
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(process.cwd(), "src"),
+    };
+    return config;
+  },
   async rewrites() {
     return [
       {
