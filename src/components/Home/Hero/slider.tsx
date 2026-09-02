a99ae09d0c521814a60fbb27fcbd6f47962253d7
Osagie Great Ayemere <mrosagiegreat@gmail.com>
2026-09-02 23:52:16 +0200

    Fix frontend Render build CSS handling

--- src/components/Home/Hero/slider.tsx
+++ src/components/Home/Hero/slider.tsx
@@ -1,8 +1,6 @@
 "use client";
 
 import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import Image from "next/image";
 import { useCryptoMarket } from "@/hooks/useCryptoMarket";
 
